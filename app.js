c5f0c9f798c160da7461087952d6c8f0b77261bf
bhanu577 <dammalapatibhanu@gmail.com>
2024-08-11 19:31:47 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const obj = require("./sum");
+
+var name = "Welcome to Java Script";
+
+var a = 10;
+var b = 20;
+
+var res = obj.calculateSum(a,b);
+
+console.log(res)
+console.log(obj.x)
+console.log(name);
+console.log(a*b);
---
+++ sum.js
@@ -0,0 +1,12 @@
+console.log("I am in sum module to add two numbers")
+
+var x = "bhanu"
+
+function calculateSum(a,b){
+return a+b ;
+}
+
+module.exports = {
+    x:x,
+    calculateSum:calculateSum
+};
