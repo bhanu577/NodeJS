95b60d84caf9d672e61a019a92a1a842197ff43e
bhanu577 <dammalapatibhanu@gmail.com>
2024-08-19 23:16:59 +0530

    add sync example

---
+++ sync.js
@@ -0,0 +1,11 @@
+console.log("hello world");
+
+var a = 10;
+var b = 20;
+
+function mutiply(a,b){
+    return a*b;
+}
+
+var res = mutiply(a,b);
+console.log("Multiplication of two number is",res);
