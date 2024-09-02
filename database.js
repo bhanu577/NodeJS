2876694c4c1597bb967b2639874d43af42a8cc0f
bhanu577 <dammalapatibhanu@gmail.com>
2024-09-02 12:43:07 +0530

    add data base connection and CURD operations

---
+++ database.js
@@ -0,0 +1,38 @@
+const { MongoClient }=require("mongodb");
+
+//connecting to clinet
+const URI = "mongodb+srv://bhanuCustom:bkJooL7Xjk22G6Kk@customlearn.zkvc4.mongodb.net/";
+const client = new MongoClient(URI);
+
+const dbName = 'Node';
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(dbName);
+  const collection = db.collection('Users');
+
+//   const insertResult = await collection.insertMany([{ firstName:'Manohar',lastName:'ch',cityName:'Vijayawada',countryName:'India' }]);
+//   console.log('Inserted documents =>', insertResult);
+
+  // the following code examples can be pasted here...
+  const findResult = await collection.find({}).toArray();
+  console.log('Found documents =>', findResult);
+
+  const updateResult =await collection.updateOne({firstName:'Bhanu'},{$set:{lastName:'prakash'}});
+  console.log('Updated Document =>',updateResult);
+
+  const removeRecord = await collection.deleteMany({firstName:'Manohar'});
+  console.log('Record Deleted =>',removeRecord);
+  
+  const findResult1 = await collection.find({}).toArray();
+  console.log('Found documents =>', findResult1);
+
+  return 'done.';
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
