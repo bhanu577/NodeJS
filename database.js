348da3cdcd85912b14029d7556964fecb7bee14b
bhanu577 <dammalapatibhanu@gmail.com>
2024-09-02 12:55:14 +0530

    add count of data base records

--- database.js
+++ database.js
@@ -13,21 +13,25 @@ async function main() {
   const db = client.db(dbName);
   const collection = db.collection('Users');
 
-//   const insertResult = await collection.insertMany([{ firstName:'Manohar',lastName:'ch',cityName:'Vijayawada',countryName:'India' }]);
-//   console.log('Inserted documents =>', insertResult);
+  const insertResult = await collection.insertMany([{ firstName:'Manohar',lastName:'ch',cityName:'Vijayawada',countryName:'India' }]);
+  console.log('Inserted documents =>', insertResult);
 
   // the following code examples can be pasted here...
-  const findResult = await collection.find({}).toArray();
-  console.log('Found documents =>', findResult);
+//   const findResult = await collection.find({}).toArray();
+//   console.log('Found documents =>', findResult);
 
-  const updateResult =await collection.updateOne({firstName:'Bhanu'},{$set:{lastName:'prakash'}});
-  console.log('Updated Document =>',updateResult);
+//   const updateResult =await collection.updateOne({firstName:'Bhanu'},{$set:{lastName:'prakash'}});
+//   console.log('Updated Document =>',updateResult);
 
-  const removeRecord = await collection.deleteMany({firstName:'Manohar'});
-  console.log('Record Deleted =>',removeRecord);
-  
-  const findResult1 = await collection.find({}).toArray();
-  console.log('Found documents =>', findResult1);
+//   const removeRecord = await collection.deleteMany({firstName:'Manohar'});
+//   console.log('Record Deleted =>',removeRecord);
+
+//   const findResult1 = await collection.find({}).toArray();
+//   console.log('Found documents =>', findResult1);
+
+
+const noOfRecords = await collection.countDocuments({});
+console.log("No Of Documents in users data base",noOfRecords);
 
   return 'done.';
 }
